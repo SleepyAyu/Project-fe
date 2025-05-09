22ca1269a8d7a5fb58cf597fedeb99057065b2d8
SleepyAyu <sirricafort@gmail.com>
2025-05-10 00:43:12 +0800

    Initial commit

---
+++ client/src/Register.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useContext, useState } from "react"
+import { UserContext } from "./UserContext";
+
+export default function Register(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+
+    async function register(ev){
+        ev.preventDefault();
+      const {data} = await  axios.post('/register', {username,password})
+      setLoggedInUsername(username)
+      setId(data.id)
+    }
+  return(
+    <div className= "bg-cyan-50 h-screen flex items-center">
+        <form className="w-64 mx-auto"
+            onSubmit={register}>
+            <input value={username}
+                onChange={ev=> setUsername(ev.target.value)}
+                type="text"
+                placeholder="username"
+                className="bg-white block w-full rounded-sm p-2 mb-2 border"/>
+            <input value={password}
+                onChange={ev=> setPassword(ev.target.value)}
+                type="password"
+                placeholder="password"
+                className="bg-white block w-full rounded-sm p-2 mb-2 border"/>
+            <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+                Register
+            </button>
+            <div className="text-center mt-2">
+              Already a member? <a href=""> Login here</a>
+            </div>
+        </form>
+    </div>
+  )  
+}
